0d80b14a23e1895e98759e1af5758d582035c68d
Alisya Odilia Shafira <alisyaodilia10@gmail.com>
2023-03-26 20:25:56 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
-"scripts": {
-    "start": "nodemon ./src/server.js",
-    "lint": "eslint ./src"
-  }, 
-
-  const Hapi = require('@hapi/hapi');
+import Hapi from '@hapi/hapi';
+import routes from './routes';
  
  
 const init = async () => {
-  const server = Hapi.server({
+  const server = _server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
- 
+
+  server.routes
  
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
