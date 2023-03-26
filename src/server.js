4446bc6deef6b64629e934d843b5a065553988c7
Alisya Odilia Shafira <alisyaodilia10@gmail.com>
2023-03-26 19:30:44 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+"scripts": {
+    "start": "nodemon ./src/server.js",
+    "lint": "eslint ./src"
+  }, 
+
+  const Hapi = require('@hapi/hapi');
+ 
+ 
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+ 
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+ 
+init();
